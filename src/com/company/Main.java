c0fee5faa955f3603d6e92981a4362c597bd98dd
Tanner Wildasin <tw073@pvsd.org>
2018-11-05 11:20:10 -0500

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.io.*;
+import java.util.*;
+import java.nio.file.Files;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Scanner sf = new Scanner(new File("/Users/tw073/IdeaProjects/twp-read-this-2018-twildasin/src/Imports.txt"));
+        int maxIndx = -1; //-1 so when we increment below, first index is 0
+        String text[] = new String[1000]; //to be safe, declare more than we need
+
+        while(sf.hasNext( ))
+        {
+            maxIndx++;
+            text[maxIndx] = sf.nextLine( ) ;
+        }
+//maxIndx is now the highest index of text[], -1 if no text lines
+        sf.close( ); //We opened a file above so close it when finished.
+
+        ///Users/tw073/IdeaProjects/twp-read-this-2018-twildasin/src/Imports.txt
+        //page 130 in Blue Pelican
     }
 }
